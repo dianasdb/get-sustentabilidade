ccc79c3b6ac0282bafa55f14128199c15704ec2b
diana <dianasilbrito@gmail.com>
2021-08-24 20:35:57 -0300

    Diana

--- src/App.tsx
+++ src/App.tsx
@@ -15,40 +15,57 @@ import carrinho from './pages/img/carrinho.png';
 function App() {
   return (
     <>
+    <div className="container">
+    <div className="header-app">
       <header>
-        <nav className="acesso">
-        <a href="/">Home</a>
-        <a href="">Quem somos?</a>
-        <a href="">Fale Conosco</a>
-        <a href="/cadastro">Cadastro</a>
+        <div className="nav-app">
+          <nav>
+            <a href="/">Home</a>
+            <a href="">Quem somos?</a>
+            <a href="">Fale Conosco</a>
+            <a href="/cadastro">Cadastro</a>
+            <a href="/carrinho"><img className="carrinho" src={carrinho} 
+            alt="Carrinho de compras"/>Carrinho</a>
+          </nav>
+        </div>
 
-        <a href="/carrinho"><img className="carrinho" src={carrinho} alt="Carrinho de compras"/>Carrinho</a>
-        </nav>
-        <form action="">
-          <input type="text" value="Palavra-Chave" />
-          <button>Buscar</button>
-        </form>
+        <div className="app-form">
+          <form action="">
+            <input type="text" value="Palavra-Chave" />
+            <button>Buscar</button>
+          </form>
+        </div>
+
       </header>
+    </div>
+
+    <div className="app-main">
       <main>
         <h1>Destaques lançamentos</h1>
         <BrowserRouter>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/annelouise" exact component={AnneLouise} />
-          <Route path="/brecho" exact component={Brecho} />
-          <Route path="/chaodecasa" exact component={ChaoCasa} />
-          <Route path="/divinafulo" exact component={DivinaFulo} />
-          <Route path="/pachamama" exact component={PachaMama} />
-          <Route path="/cadastro" exact component={Cadastro} />
-          <Route path="/carrinho" exact component={Carrinho} />
-        </Switch>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/annelouise" exact component={AnneLouise} />
+            <Route path="/brecho" exact component={Brecho} />
+            <Route path="/chaodecasa" exact component={ChaoCasa} />
+            <Route path="/divinafulo" exact component={DivinaFulo} />
+            <Route path="/pachamama" exact component={PachaMama} />
+            <Route path="/cadastro" exact component={Cadastro} />
+            <Route path="/carrinho" exact component={Carrinho} />
+          </Switch>
         </BrowserRouter>
       </main>
-      <footer>
-        Contato
-        E-mail:
-        Telefone:
-      </footer>
+    </div>
+
+      <div className="footer-app">
+        <footer>
+          Contato
+          E-mail:
+          Telefone:
+        </footer>
+      </div>
+    </div>
+
    </>
   );
 }
